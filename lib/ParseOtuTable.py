2aaf68035810f26e05df804e65d4f2ac2a0d4ea6
ZeweiSong <songx208@umn.edu>
2015-04-24 12:58:36 -0500

    parse otu table

--- lib/ParseOtuTable.py
+++ lib/ParseOtuTable.py
@@ -13,37 +13,66 @@ songzewei@outlook.com
 
 
 class parser_otu_table(object):
-    def __init__(self, file_path):
+    def __init__(self, file_path, meta_col = 'taxonomy'):
         with open(file_path, 'rU') as f:
             temp = f.readlines()
         table = []
         for line in temp:
             table.append(line.strip('\n').split('\t'))
-        self.sample_id = table[0][1:]
-        self.species_id = [i[0] for i in table[1:]]
-        self.species_matrix = table[1:]
-        temp = [i[1:] for i in self.species_matrix]
+        
+        # Get id for sample, species, amd meta data
+        try:        
+            meta_position = table[0].index(meta_col)  # Begining position of meta data
+        except ValueError:
+            meta_position = len(table[0]) + 1
+        self.sample_id = table[0][1:meta_position]  # Second column till the first meta column
+        self.meta_id = table[0][meta_position:]  # Start from the first meta column to the end
+        self.species_id = [i[0] for i in table[1:]]  # First column stating from the second row
+        
+        # Get sample, species, and meta data matrix with head names
+        self.species_matrix = [i[:meta_position] for i in table[1:]]
+        temp = [i[1:meta_position] for i in table]
         self.sample_matrix = [list(i) for i in zip(*temp)]
-        for i in range(len(self.sample_matrix)):
-            self.sample_matrix[i] = [self.sample_id[i]] + self.sample_matrix[i]
-
+        temp = [i[meta_position:] for i in table]
+        self.meta_matrix = [list(i) for i in zip(*temp)]
+        
+        # Check abundance value in the OTU table
+        try:
+            for line in self.sample_matrix[1:]:
+                temp = [int(i) for i in line]
+        except ValueError:
+            print "There are non-number value in your OTU table."
+            import sys
+            sys.exit()
+        
+    # Generate a dicionary using sample name, OTU name
     def sample_dict(self):
         sample = {}
         for s in self.sample_id:
             sample[s] = {}
-        for line in self.species_matrix:
-            for i in range(len(self.sample_id)):
+        for line in self.sample_matrix:
+            for i in range(len(self.species_id)):
                 if int(line[1:][i]) > 0:
-                    sample[self.sample_id[i]][line[0]] = int(line[1:][i])
+                    sample[line[0]][self.species_id[i]] = int(line[1:][i])
         return sample
 
-
+    # Generate a dicionry using OTU name, sample name
     def species_dict(self):
         species = {}
         for s in self.species_id:
             species[s] = {}
-        for line in self.sample_matrix:
-            for i in range(len(self.species_id)):
+        for line in self.species_matrix:
+            for i in range(len(self.sample_id)):
                 if int(line[1:][i]) > 0:
-                    species[self.species_id[i]][line[0]] = int(line[1:][i])
+                    species[line[0]][self.sample_id[i]] = int(line[1:][i])
         return species
+
+    
+    # Generate a dictionary using mata name, OTU name
+    def meta_dict(self):
+        meta = {}
+        for m in self.meta_id:
+            meta[m] = {}
+        for line in self.meta_matrix:
+            for i in range(len(self.species_id)):
+                meta[line[0]][self.species_id[i]] = line[1:][i]
