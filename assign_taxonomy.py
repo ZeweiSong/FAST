7037cf42c84b13286675a4a0eb9a4122c7bca977
ZeweiSong <songx208@umn.edu>
2015-04-24 11:45:05 -0500

    assign taxonomy

---
+++ assign_taxonomy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 24 11:44:11 2015
+
+Assign taxonomy information to the OTU table.
+
+Please feel free to contact me for any question.
+--
+Zewei Song
+University of Minnesota
+Dept. Plant Pathology
+songzewei@outlook.com
+"""
+
