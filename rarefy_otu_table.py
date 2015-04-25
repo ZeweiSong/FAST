0b7e929a7ac92b3cd8345a1dac5853338d0b5a4e
ZeweiSong <songx208@umn.edu>
2015-04-24 19:35:55 -0500

    argument parser

--- rarefy_otu_table.py
+++ rarefy_otu_table.py
@@ -10,3 +10,13 @@ Dept. Plant Pathology
 songzewei@outlook.com
 """
 
+from lib import random_subsample
+from lib import ParseOtuTable
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-otu', help='Input OTU table')
+parser.add_argument('-d', '--depth', help='Sampling depth for each sample')
+parser.add_argument('-iter', default=1, help='Iteration time for each sample')
+parser.add_argument('-keep_all', action='store_true', help='Indicate to keep all samples')
+args = parser.parse_args()
