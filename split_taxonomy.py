f323a68baf910d261200819c5314941351059593
Zewei Song <songzewei@outlook.com>
2015-11-20 14:56:50 -0600

    new script, split_taxonomy

---
+++ split_taxonomy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 20 14:54:48 2015
+
+This scrtipt with output two files with the abundance and richess of the given taxonomic level.
+
+Please feel free to contact me with any question.
+--
+Zewei Song
+University of Minnesota
+Dept. Plant Pathology
+songzewei@outlook.com
+"""
+
