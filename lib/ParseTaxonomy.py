a15fe2a858268b6d33f47731b234e0e48918d7c2
Zewei Song <songzewei@outlook.com>
2015-11-20 13:47:30 -0600

    New lib, ParseTaxonomy

---
+++ lib/ParseTaxonomy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 20 13:43:00 2015
+
+This library will parse the taxonomy in an OTU table (tab delimited) into a given taxonomic level.
+
+Please feel free to contact me with any question.
+--
+Zewei Song
+University of Minnesota
+Dept. Plant Pathology
+songzewei@outlook.com
+"""
+
