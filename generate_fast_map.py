b3a7f027fd2c520f9dd0176095eeeebc002d34a9
Zewei Song <songzewei@outlook.com>
2016-03-02 09:03:41 -0600

    a new script for generate FAST style map

---
+++ generate_fast_map.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 02 09:01:07 2016
+
+This script will generate a new FAST style map file. This map file is similar to a Qiime style OTU map,
+but also contains the information about sequences. It could be a map for either dereplicated seuqences,
+or OTUs. If it is for OTUs, it can also contain information about the detail dereplicated unit within each
+OTUs (abudnance and sequence)
+
+Please feel free to contact me for any question.
+--
+Zewei Song
+University of Minnesota
+Dept. Plant Pathology
+songzewei@outlook.com
+www.songzewei.org
+"""
+
