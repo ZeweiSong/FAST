d7758deeb2c7290f5ca064932d97abd0269aa69d
Zewei Song <songzewei@outlook.com>
2016-03-02 20:53:10 -0600

    add function to the main script

--- fast.py
+++ fast.py
@@ -13,79 +13,48 @@ Dept. Plant Pathology
 songzewei@outlook.com
 www.songzewei.org
 """
-
-import argparse
-import sys
-#print sys.argv
-parser = argparse.ArgumentParser(prog='fast.py -function', add_help=False)
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('-document', action='store_true', help='Print out the helping document for the main program.')
-group.add_argument('-generate_mapping', action = "store_true")
-group.add_argument('-add_labels', action = "store_true")
-group.add_argument('-merge_seqs', action = "store_true")
-group.add_argument('-filter_seqs', action = "store_true")
-group.add_argument('-dereplicate', action = "store_true")
-group.add_argument('-filter_otu_map', action = "store_true")
-group.add_argument('-add_seqs_size', action = "store_true")
-group.add_argument('-parse_uc_cluster', action = "store_true")
-group.add_argument('-parse_uparse_cluster', action = "store_true")
-group.add_argument('-generate_fast_map', action = "store_true")
-group.add_argument('-combine_fast_map', action = "store_true")
-group.add_argument('-rename_otu_map', action = "store_true")
-
-#args = parser.parse_known_args(['-option1','songzewei','-sub','soon'])
-args = parser.parse_args([sys.argv[1]])
-for option in args.__dict__:
-    if args.__dict__[option]:
-        function_name = option
-
-sub_args = sys.argv[2:]
-
-if args.document:
-    print "This is the helping document:"
-
-else:
-    function = __import__(function_name)
-    function.main(sub_args)
-
-'''    
-if args.generate_mapping:
-    import generate_mapping
-    generate_mapping.main(sub_args)
+def main():
+    import argparse
+    import sys
+    #print sys.argv
+    parser = argparse.ArgumentParser(prog='fast.py -function', add_help=False)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-document', action='store_true', help='Print out the helping document for the main program.')
+    group.add_argument('-generate_mapping', action = "store_true")
+    group.add_argument('-add_labels', action = "store_true")
+    group.add_argument('-merge_seqs', action = "store_true")
+    group.add_argument('-filter_seqs', action = "store_true")
+    group.add_argument('-dereplicate', action = "store_true")
+    group.add_argument('-filter_otu_map', action = "store_true")
+    group.add_argument('-add_seqs_size', action = "store_true")
+    group.add_argument('-parse_uc_cluster', action = "store_true")
+    group.add_argument('-parse_uparse_cluster', action = "store_true")
+    group.add_argument('-generate_fast_map', action = "store_true")
+    group.add_argument('-combine_fast_map', action = "store_true")
+    group.add_argument('-rename_otu_map', action = "store_true")
+    group.add_argument('-stat_seqs', action = 'store_true')
+    group.add_argument('-pick_seqs', action = 'store_true')
     
-if args.add_labels:
-    import add_labels
-    add_labels.main(sub_args)
+    #args = parser.parse_known_args(['-option1','songzewei','-sub','soon'])
+    args = parser.parse_args([sys.argv[1]])
+    for option in args.__dict__:
+        if args.__dict__[option]:
+            function_name = option
+    
+    sub_args = sys.argv[2:]
+    
+    if args.document:
+        print "This is the helping document:"
+    
+    else:
+        function = __import__(function_name)
+        function.main(sub_args)
+    
+    '''    
+    if args.generate_mapping:
+        import generate_mapping
+        generate_mapping.main(sub_args)
+    '''
 
-if args.merge_seqs:
-    import merge_seqs
-    merge_seqs.main(sub_args)
-
-if args.filter_seqs:
-    import filter_seqs
-    filter_seqs.main(sub_args)
-
-if args.dereplicate:
-    import dereplicate
-    dereplicate.main(sub_args)
-
-if args.filter_otu_map:
-    import filter_otu_map
-    filter_otu_map.main(sub_args)
-
-if args.add_seqs_size:
-    import add_seqs_size
-    add_seqs_size.main(sub_args)
-
-if args.parse_uc_cluster:
-    import parse_uc_cluster
-    parse_uc_cluster.main(sub_args)
-
-if args.parse_uparse_cluster:
-    import parse_uparse_cluster
-    parse_uparse_cluster.main(sub_args)
-
-if args.generate_fast_map:
-    import generate_fast_map
-    generate_fast_map.main(sub_args)
-'''
+if __name__ == '__main__':
+    main()
--- pick_seqs.py
+++ pick_seqs.py
@@ -15,63 +15,75 @@ University of Minnesota
 Dept. Plant Pathology
 songzewei@outlook.com
 """
+def main(name_space):
+    import argparse
+    import textwrap
+    from lib import ParseOtuMap
+    from lib import Seq_IO
+    from lib import File_IO
+    
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog=textwrap.dedent('''\
+                                    ------------------------
+                                    By Zewei Song
+                                    University of Minnesota
+                                    Dept. Plant Pathology
+                                    songzewei@outlook.com
+                                    ------------------------'''), prog='fast.py -pick_seqs')
+    parser.add_argument('-i', '--input', help='Input FASTA file to be picked')
+    parser.add_argument('-o', '--output', help='Name for output FASTA file.')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-map', help='OTU map file, will pick OTU names in default')
+    group.add_argument('-name_list', help='File with names in separated lines')
+    parser.add_argument('-sequence', action='store_true', help='Indicate to pick by sequence names instead of OTU names')
+    args = parser.parse_args(name_space)
+    
+    input_fasta = args.input
+    output_fasta = args.output
+    pick_list = []
+    if args.map:
+        if not args.sequence:
+            otu_map = ParseOtuMap.read_otu_map(args.map)
+            for key in otu_map:
+                pick_list.append(key)
+        elif args.sequence:
+            otu_map = ParseOtuMap.read_otu_map(args.map)
+            for key, value in otu_map.items():
+                pick_list += value
+    if args.name_list:
+        with open(args.name_list, 'rU') as f:
+            for line in f:
+                pick_list.append(line.strip('\n'))
+    print 'Found %i names to be picked.' % len(pick_list)
+    
+    print 'Reaing in the original FASTA file: %s ...' % input_fasta
+    input_content = File_IO.read_seqs(input_fasta)
+    for record in input_content:
+        record[0] = record[0].split(' ')[0]  # OTU name will be cut at the first space
+    print 'Indexing the original sequence file ...'
+    input_dict = Seq_IO.make_dict(input_content)
+    
+    count_picked = 0
+    count_missed = 0
+    print 'Search name list in the sequence file ...'
+    picked_content = []
+    for name in pick_list:
+        try:
+            picked_content.append([name, input_dict[name][0]])
+            count_picked += 1
+        except KeyError:
+            count_missed += 1
+    
+    print 'Finished searching.'
+    print 'Original sequence=%i' % len(input_content)
+    print 'Input names=%i' % len(pick_list)
+    print 'Picked sequences=%i' % count_picked
+    print 'Not found sequences=%i' % count_missed
+    
+    print 'Writing to a new FASTA file ...'
+    count = File_IO.write_seqs(picked_content, output_fasta, checker=False, overwrite=True)
+    print 'Picked sequences wrote to %s.' % output_fasta
 
-import argparse
-from lib import ParseOtuMap
-from lib import Seq_IO
-from lib import File_IO
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--input', help='Input FASTA file to be picked')
-parser.add_argument('-o', '--output', help='Name for output FASTA file.')
-group = parser.add_mutually_exclusive_group()
-group.add_argument('-map', help='OTU map file, will pick OTU names in default')
-group.add_argument('-name_list', help='File with names in separated lines')
-parser.add_argument('-sequence', action='store_true', help='Indicate to pick by sequence names instead of OTU names')
-args = parser.parse_args()
-
-input_fasta = args.input
-output_fasta = args.output
-pick_list = []
-if args.map:
-    if not args.sequence:
-        otu_map = ParseOtuMap.read_otu_map(args.map)
-        for key in otu_map:
-            pick_list.append(key)
-    elif args.sequence:
-        otu_map = ParseOtuMap.read_otu_map(args.map)
-        for key, value in otu_map.items():
-            pick_list += value
-if args.name_list:
-    with open(args.name_list, 'rU') as f:
-        for line in f:
-            pick_list.append(line.strip('\n'))
-print 'Found %i names to be picked.' % len(pick_list)
-
-print 'Reaing in the original FASTA file: %s ...' % input_fasta
-input_content = File_IO.read_seqs(input_fasta)
-for record in input_content:
-    record[0] = record[0].split(' ')[0]  # OTU name will be cut at the first space
-print 'Indexing the original sequence file ...'
-input_dict = Seq_IO.make_dict(input_content)
-
-count_picked = 0
-count_missed = 0
-print 'Search name list in the sequence file ...'
-picked_content = []
-for name in pick_list:
-    try:
-        picked_content.append([name, input_dict[name][0]])
-        count_picked += 1
-    except KeyError:
-        count_missed += 1
-
-print 'Finished searching.'
-print 'Original sequence=%i' % len(input_content)
-print 'Input names=%i' % len(pick_list)
-print 'Picked sequences=%i' % count_picked
-print 'Not found sequences=%i' % count_missed
-
-print 'Writing to a new FASTA file ...'
-count = File_IO.write_seqs(picked_content, output_fasta, checker=False, overwrite=True)
-print 'Picked sequences wrote to %s.' % output_fasta
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
