3baacd98b00bb3f2267e85cfb23b4d1ed176afed
Zewei Song <songzewei@outlook.com>
2016-03-04 16:00:35 -0600

    main function

--- fast.py
+++ fast.py
@@ -15,13 +15,13 @@ www.songzewei.org
 """
 def main():
     import argparse
-    import textwrap
+    #import textwrap
     import sys
     #import importlib
     
     parser = argparse.ArgumentParser(prog='fast.py -function', add_help=False)
     
-    group = parser.add_mutually_exclusive_group(required=True)
+    group = parser.add_mutually_exclusive_group(required=False)
     group.add_argument('-add_labels', action = "store_true")
     group.add_argument('-add_seqs_size', action = "store_true")
     group.add_argument('-assign_taxonomy', action = 'store_true')
@@ -52,12 +52,18 @@ def main():
     group.add_argument('-substract_controls', action = 'store_true')
     group.add_argument('-truncate_seqs', action = 'store_true')
     
-    args = parser.parse_args([sys.argv[1]])
-    #    for option in args.__dict__:
-    #        if args.__dict__[option]:
-    #            function_name = option
+    if len(sys.argv) <= 1:
+        print "This is the helping document:"
+        sys.exit()
     
-    sub_args = sys.argv[2:]
+    else:
+        args = parser.parse_args([sys.argv[1]])
+#        function_name = False
+#        for option in args.__dict__:
+#            if args.__dict__[option]:
+#                function_name = option
+       
+        sub_args = sys.argv[2:]
     
     if args.document:
         print "This is the helping document:"
@@ -75,17 +81,120 @@ def main():
     #        function.main(sub_args)
         
     if args.add_labels:
-        import add_labels as function
-        print sub_args
-        function.main(sub_args)
+    	import add_labels as function
+    	function.main(sub_args)
     
-    if args.generate_mapping:
-        import generate_mapping as function
-        function.main(sub_args)
+    if args.add_seqs_size:
+    	import add_seqs_size as function
+    	function.main(sub_args)
+    
+    if args.assign_taxonomy:
+    	import assign_taxonomy as function
+    	function.main(sub_args)
+    
+    if args.combine_fast_map:
+    	import combine_fast_map as function
+    	function.main(sub_args)
+    
+    if args.convert_fastq:
+    	import convert_fastq as function
+    	function.main(sub_args)
+    
+    if args.correct_fasta:
+    	import correct_fasta as function
+    	function.main(sub_args)
+    
+    if args.count_seqs:
+    	import count_seqs as function
+    	function.main(sub_args)
     
     if args.dereplicate:
-        import dereplicate as function
-        function.main(sub_args)
+    	import dereplicate as function
+    	function.main(sub_args)
+    
+    if args.document:
+    	import document as function
+    	function.main(sub_args)
+    
+    if args.filter_database:
+    	import filter_database as function
+    	function.main(sub_args)
+    
+    if args.filter_otu_map:
+    	import filter_otu_map as function
+    	function.main(sub_args)
+    
+    if args.filter_seqs:
+    	import filter_seqs as function
+    	function.main(sub_args)
+    
+    if args.generate_fast_map:
+    	import generate_fast_map as function
+    	function.main(sub_args)
+    
+    if args.generate_mapping:
+    	import generate_mapping as function
+    	function.main(sub_args)
+    
+    if args.make_otu_table:
+    	import make_otu_table as function
+    	function.main(sub_args)
+    
+    if args.merge_otu_maps:
+    	import merge_otu_maps as function
+    	function.main(sub_args)
+    
+    if args.merge_seqs:
+    	import merge_seqs as function
+    	function.main(sub_args)
+    
+    if args.otu_deconstruct:
+    	import otu_deconstruct as function
+    	function.main(sub_args)
+    
+    if args.otu_map_info:
+    	import otu_map_info as function
+    	function.main(sub_args)
+    
+    if args.parse_uc_cluster:
+    	import parse_uc_cluster as function
+    	function.main(sub_args)
+    
+    if args.parse_uparse_cluster:
+    	import parse_uparse_cluster as function
+    	function.main(sub_args)
+    
+    if args.pick_seqs:
+    	import pick_seqs as function
+    	function.main(sub_args)
+    
+    if args.random_dataset:
+    	import random_dataset as function
+    	function.main(sub_args)
+    
+    if args.rarefy_otu_table:
+    	import rarefy_otu_table as function
+    	function.main(sub_args)
+    
+    if args.rename_otu_map:
+    	import rename_otu_map as function
+    	function.main(sub_args)
+    
+    if args.split_taxa:
+    	import split_taxa as function
+    	function.main(sub_args)
+    
+    if args.stat_seqs:
+    	import stat_seqs as function
+    	function.main(sub_args)
+    
+    if args.substract_controls:
+    	import substract_controls as function
+    	function.main(sub_args)
+    
+    if args.truncate_seqs:
+    	import truncate_seqs as function
+    	function.main(sub_args)
 
 if __name__ == '__main__':
     main()
