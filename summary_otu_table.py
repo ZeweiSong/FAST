99d21a66dbf75ca9134d4c9059e436404183b0a0
Zewei Song <songzewei@outlook.com>
2016-04-15 16:06:32 -0500

    finish the function

--- summary_otu_table.py
+++ summary_otu_table.py
@@ -17,7 +17,7 @@ www.songzewei.org
 def main():
     import argparse
     import textwrap
-    from lib import File_IO
+    from lib import ParseOtuTable
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
                                      epilog=textwrap.dedent('''\
@@ -27,9 +27,27 @@ def main():
                                     Dept. Plant Pathology
                                     songzewei@outlook.com
                                     ------------------------'''), prog='fast.py -summary_otu_table')
-    parser.add_argument('-otu', '--input', help='Name of the input OTU table.')
+    parser.add_argument('-otu', help='Name of the input OTU table.')
     parser.add_argument('-o', '--output', default='otu_report.txt', help='Name of the output report.')
     args = parser.parse_args()
+    
+    input_file = args.otu
+    output_file = args.output
+    
+    otu_table = ParseOtuTable.parser_otu_table(input_file)
+
+    sample_id = otu_table.sample_id
+    sample_dict = otu_table.sample_dict()    
+    report_dict = {}    
+    for sample in sample_id:
+        report_dict[sample] = sum(sample_dict[sample].values())
+    
+    with open(output_file, 'w') as f:
+        f.write('Sample\tDepth\n')
+        for sample in sample_id:
+            line = ''
+            line = sample + '\t' + str(report_dict[sample])
+            f.write('%s\n' % line)
 
 if __name__ == '__main__':
     main()
