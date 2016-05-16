4e9db6b9f92f1428b0642bf939f550140e6ee197
Zewei Song <songzewei@outlook.com>
2016-05-16 14:19:57 -0500

    finish the function

--- nucl_freq.py
+++ nucl_freq.py
@@ -3,6 +3,8 @@
 """
 Created on Wed Apr 13 20:36:50 2016
 
+Calculte the frequency of nucleotide from the starting of sequences.
+
 Please feel free to contact me with any question.
 --
 Zewei Song
@@ -27,6 +29,49 @@ def main(Namespace):
     parser.add_argument('-i', '--input', help='Name of the input FASTA or FASTQ file.')
     parser.add_argument('-o', '--output', default='nucl_report.txt', help='Name of the reporting file.')
     args = parser.parse_args(Namespace)
+    
+    input_file = args.input
+    output_file = args.output
+    
+    input_seq = File_IO.read_seqs(input_file)
+    
+    # Get the maximum sequence length
+    seq_len = []
+    for record in input_seq:
+        seq_len.append(len(record[1]))
+    max_seq_len = max(seq_len)
+    
+    # Create dictionary for nucleotide counting    
+    nucl_dict = {}
+    nucl_list = ['A','T','C','G','N']
+    for i in range(max_seq_len):
+        nucl_dict[i] = {}
+        for nucl in nucl_list:
+            nucl_dict[i][nucl] = 0
+
+    # Count the frequency of nucleotide    
+    for record in input_seq:
+        for index, nucl in enumerate(record[1]):
+            try:
+                nucl_dict[index][nucl] += 1
+            except KeyError:
+                print "Unidentified nucleotide %s in %s" % (nucl, record[0])
+    
+    # Output the counting result
+    header = 'Position\tA\tT\tC\tG\tN'
+    output_content = [header]
+    for pos in range(max_seq_len):
+        current_line = []
+        current_line = [str(pos)]
+        for nucl in nucl_list:
+            current_line.append(str(nucl_dict[pos][nucl]))
+        current_line = '\t'.join(current_line)
+        output_content.append(current_line)
+    
+    with open(output_file, 'wb') as f:
+        for line in output_content:
+            f.write("%s\n" %line)
+    
 
 if __name__ == '__main__':
     import sys
