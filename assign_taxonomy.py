a249bb34b797fded45ec8670e46d61031878cd3e
Zewei Song <songzewei@outlook.com>
2017-02-24 17:17:58 -0600

    Add new combined function for filter taxa

--- assign_taxonomy.py
+++ assign_taxonomy.py
@@ -14,6 +14,7 @@ University of Minnesota
 Dept. Plant Pathology
 songzewei@outlook.com
 """
+from __future__ import print_function
 def main(name_space):
     from lib import ParseOtuTable
     import argparse
@@ -27,12 +28,16 @@ def main(name_space):
                                         Dept. Plant Pathology
                                         songzewei@outlook.com
                                         ------------------------'''), prog = '-assign_taxonomy')
-    parser.add_argument("-otu", help="Input OTU folder")
-    parser.add_argument("-tax", help="BLAST taxonomy search result")
+    parser.add_argument("-otu", help="Input OTU table")
+    parser.add_argument("-tax", help="Taxonomy search result in blast6out format: query+target+ql+pairs+id")
     parser.add_argument("-o", "--output", help="Output OTU table")
-    parser.add_argument('-scores', action='store_true', help='Indicate to output detail matching score following the taxonomy column.')
-    parser.add_argument('-remove_unassigned', action='store_true', help='Indicate to remove OTUs without a taxonomy assignment.')
-    #parser.add_argument('-id', help='This is the similarity threshold to keep a taxonomy record, assignments with lower id will be filled with no_hit.')
+    parser.add_argument('-scores', action='store_true', help='Indicate to output matching scores following the taxonomy column.')
+    parser.add_argument('-keep_all', action='store_true', help='Indicate to keep all OTUs including low confidencial ones.')
+    parser.add_argument('-min_match', default = 0.0, help='Threshod for minimum match length to keep in OTU table.')
+    parser.add_argument('-min_pident', default = 0.0, help='Threshod for minimum pident to keep in OTU table.')
+    parser.add_argument('-match_length', default = 0.0, help='Threshold for matched length that can be assigned a taxa.')
+    parser.add_argument('-pident', default = 97, help='Threshold for pident that can be assigned a taxa.')
+    
     args = parser.parse_args(name_space)
     
     input_otu = args.otu
@@ -41,54 +46,78 @@ def main(name_space):
     
     otu_table = ParseOtuTable.parser_otu_table(input_otu)
     
-    # Read in BLAST 6 style taxonomy result and store in a dictionary
-    taxonomy_temp = []
+    # Read in BLAST 6 style taxonomy result and store in a dictionary (the out has to be arrange as query+target+ql+pairs+id )
+    input_content = []
     with open(input_tax, 'rU') as f:
-        if args.scores:
-            for line in f:
-                taxonomy_temp.append(line.strip('\n').split('\t'))
-        else:
-            for line in f:
-                taxonomy_temp.append(line.strip('\n').split('\t')[:2])
+        for line in f:
+            line = line.strip('\n').split('\t')
+            input_content.append(line)
+            
+   
     taxonomy = {}
-    for line in taxonomy_temp:
+    for line in input_content:
         taxonomy[line[0]] = line[1:]
     
+    # Filther the taxonomy result using the minimal threshold (These records are likely to be in the targeted group, such as fungi).
+    taxonomy_keep = {}
+    low_confident_count = 0
+    for key, value in taxonomy.items():
+        match_score = float(int(value[2]))/int(value[1])
+        
+        pident_score = float(value[3])
+        #print([match_score, pident_score, args.min_match, args.min_pident])
+        if match_score >= float(args.min_match) and pident_score >= float(args.min_pident):
+            taxonomy_keep[key] = value
+        else:
+            low_confident_count += 1
+    
+    taxonomy_assign = {}
+    no_hit_count = 0
+    for key, value in taxonomy_keep.items():
+        match_score = float(int(value[2]))/int(value[1])
+        pident_score = float(value[3])
+        if match_score >= float(args.match_length) and pident_score >= float(args.pident):
+            taxonomy_assign[key] = value
+        else:
+            no_hit_count += 1
+    
+    print(len(taxonomy))
+    print(len(taxonomy_keep))
+    print(len(taxonomy_assign))
+
     otu_matrix = otu_table.species_matrix
     otu_matrix_assigned = []
-    if args.remove_unassigned:
-        print 'OTUs without taxonomy assignment will be removed.'
-        hit = 0
-        no_hit = 0
-        for line in otu_matrix:
-            try:
-                line += taxonomy[line[0]]
-                otu_matrix_assigned.append(line)
-                hit += 1
-            except KeyError:
-                no_hit += 1
-    else:
-        print 'OTUs without taxonomy assignment will be kept.'
-        hit = 0
-        no_hit = 0
-        for line in otu_matrix:
-            try:
-                line += taxonomy[line[0]]
-                otu_matrix_assigned.append(line)
-                hit += 1
-            except KeyError:
-                line.append('no_hit')
-                otu_matrix_assigned.append(line)
-                no_hit += 1
     
-    print '{0} OTUs have been assigned a taxa.'.format(hit)
-    print '{0} OTUs have no hit.'.format(no_hit)
+    low_confident_otu = 0
+    no_hit_otu = 0
+    hit_otu = 0
     
+    for line in otu_matrix:
+        #print(taxonomy_keep[line[0]])
+        try:
+            line += taxonomy_assign[line[0]] + ['above_assignment_threshold']
+            otu_matrix_assigned.append(line)
+            hit_otu += 1
+        except KeyError:
+            try:
+                line += taxonomy_keep[line[0]] + ['below_assignment_threshold']
+                otu_matrix_assigned.append(line)
+                no_hit_otu += 1
+            except KeyError:
+                if args.keep_all:
+                    try:
+                        line += taxonomy[line[0]] + ['below_minimum_threshold']
+                        otu_matrix_assigned.append(line)
+                        low_confident_otu += 1
+                    except KeyError:
+                        line += ['nan','nan','nan','nan','below_minimum_threshold']
+                        otu_matrix_assigned.append(line)
+                        low_confident_otu += 1
+                else:
+                    pass
+
     sample_id = otu_table.sample_id
-    if args.scores:
-        sample_id = ['OTU_ID'] + sample_id + ['taxonomy','Query_Len','Subject_Len','Pident','Evalue']
-    else:
-        sample_id = ['OTU_ID'] + sample_id + ['taxonomy']
+    sample_id = ['OTU_ID'] + sample_id + ['taxonomy','Query_length','Query_aligned_length','Pident','Threshold']
     
     # Write the new OTU table
     with open(output_otu, 'w') as f:
@@ -97,7 +126,7 @@ def main(name_space):
             line = [str(x) for x in line]
             f.write('%s\n' % '\t'.join(line))
     
-    print 'The new OTU table was saved in {0}.'.format(output_otu)
+    print('The new OTU table was saved in {0}.'.format(output_otu))
     
 if __name__ == '__main__':
     import sys
