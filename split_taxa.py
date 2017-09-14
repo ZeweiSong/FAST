6bb40b6933111084f47d2c43d2ecd45e922b9c30
Zewei Song <songzewei@outlook.com>
2017-09-14 15:05:54 -0500

    adopt to Python 3

--- split_taxa.py
+++ split_taxa.py
@@ -12,7 +12,8 @@ University of Minnesota
 Dept. Plant Pathology
 songzewei@outlook.com
 """
-
+from __future__ import print_function
+from __future__ import division
 def main(name_space):
     import argparse
     import textwrap
@@ -30,16 +31,16 @@ def main(name_space):
     parser.add_argument("-tax", help="Taxonomic level from k, p, c, o, f, g, s")
     args = parser.parse_args(name_space)
     tax_dict = {'k':'Kingdom', 'p':'Phylum', 'c':'Class', 'o':'Order', 'f':'Family', 'g':'Genus', 's':'Species'}
-    
-    print 'Splitting OTU table: {0} at the level of {1} ...'.format(args.otu, tax_dict[args.tax])
+
+    print('Splitting OTU table: {0} at the level of {1} ...'.format(args.otu, tax_dict[args.tax]))
     split_tax_table = ParseTaxonomy.taxonomy_info(args.otu, args.tax)
     tax_number = len(split_tax_table['abundance'])
     split_tax_table_output = ParseTaxonomy.taxonomy_output(split_tax_table)
-    
+
     output_abundance = 'abundance_' + args.tax + '_' + args.otu
     output_richness = 'richness_' + args.tax + '_' + args.otu
 
-    # Write the abundance data    
+    # Write the abundance data
     temp_abundance = []
     for line in split_tax_table_output['abundance'][1:]:
         temp_sum = sum([int(i) for i in line[1:]])
@@ -55,14 +56,14 @@ def main(name_space):
             line = [str(i) for i in line]
             line = '\t'.join(line)
             f.write('%s\n' % line)
-    
+
     # Write the richness data
     temp_richness = []
     for line in split_tax_table_output['richness'][1:]:
         temp_sum = sum([int(i) for i in line[1:]])
         line = [temp_sum] + line
         temp_richness.append(line)
-    temp_richness = sorted(temp_richness, reverse = True)                        
+    temp_richness = sorted(temp_richness, reverse = True)
     with open(output_richness, 'w') as f:
         header = split_tax_table_output['richness'][0]
         header = '\t'.join(header)
@@ -72,12 +73,12 @@ def main(name_space):
             line = [str(i) for i in line]
             line = '\t'.join(line)
             f.write('%s\n' % line)
-    
 
-    print 'Find %i %s.' % (tax_number, tax_dict[args.tax])
-    print 'Splitted OTU table by abundance saved in %s.' % output_abundance
-    print 'Splitted OTU table by richness saved in %s.' % output_richness
-            
+
+    print('Find %i %s.' % (tax_number, tax_dict[args.tax]))
+    print('Splitted OTU table by abundance saved in %s.' % output_abundance)
+    print('Splitted OTU table by richness saved in %s.' % output_richness)
+
 if __name__ == '__main__':
     import sys
     main(sys.argv[1:])
